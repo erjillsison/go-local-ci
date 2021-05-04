3d45df42df7d0bf41fa65b0bbe4836635098a97f
Erjill Sison <erjill.sison@C02C97E5MD6M.local>
2021-05-04 18:48:38 +0800

    enable sonarqube through toggle; fix golangci-lint issue

--- go-local-ci.sh
+++ go-local-ci.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
 #cd $1
-OUTDIR="sonarout"
+OUTDIR="lint"
+
+# Set ENABLE_SONARQUBE to 1 to run sonar-scanner and upload results to sonarqube
+ENABLE_SONARQUBE=0
+
 echo $(pwd)
 echo "creating temp dir $OUTDIR"
 mkdir -p $OUTDIR
-echo "running tests..."
-go test -coverprofile=$OUTDIR/coverage.out -race $(go list ./...)
-echo "running golangci-lint..."
-golangci-lint run --timeout=3m0s \
-# --out-format checkstyle \ # use this if uploading to SonarQube
-$(if [ ! -f .golangci.yml ]; 
-    then echo "--enable goimports,golint,dupl,exportloopref,goconst,bodyclose,dogsled,funlen,misspell,unparam"; 
-fi) ./... | tee "$OUTDIR/gcilint.out"
 
-# For sonarqube display
-# docker run \
-#     --rm \
-#     -e SONAR_HOST_URL="$SONARQUBE_URL" \
-#     -e SONAR_LOGIN="$SONARQUBE_LOGIN" \
-#     -v "$(pwd):/usr/src" \
-#     sonarsource/sonar-scanner-cli \
-#     -Dsonar.projectKey="$(echo $(basename $(pwd)):$(git rev-parse --abbrev-ref HEAD) | sed -e 's/[^A-Za-z0-9._:-]/-/g')" \
-#     -Dsonar.go.golangci-lint.reportPaths="$OUTDIR/gcilint.out" \
-#     -Dsonar.go.coverage.reportPaths="$OUTDIR/coverage.out" \
-#     -Dsonar.exclusions="spex/gen/**/*"
+echo "running tests..."
+go test -race \
+-coverprofile=$OUTDIR/coverage.out \
+$(go list ./...)
+
+echo "running golangci-lint..."
+golangci-lint run --verbose --timeout=3m0s \
+$(if [ "$ENABLE_SONARQUBE" -eq "1" ]; then
+   echo "--out-format checkstyle";
+fi) \
+$(if [ ! -f .golangci.yml ]; then 
+    echo "--enable goimports,golint,dupl,exportloopref,goconst,bodyclose,dogsled,funlen,misspell,unparam";
+fi) \
+./... | tee "$OUTDIR/gcilint.out"
+
+
+if [ "$ENABLE_SONARQUBE" -eq "1" ]; then
+    echo "SONAR_HOST_URL: $SONAR_HOST_URL"
+    echo "SONAR_LOGIN_KEY: $SONAR_LOGIN"
+
+    docker run \
+    --rm \
+    -e SONAR_HOST_URL="$SONAR_HOST_URL" \
+    -e SONAR_LOGIN="$SONAR_LOGIN" \
+    -v "$(pwd):/usr/src" \
+    sonarsource/sonar-scanner-cli \
+    -Dsonar.projectKey="$(echo $(basename $(pwd)):$(git rev-parse --abbrev-ref HEAD) | sed -e 's/[^A-Za-z0-9._:-]/-/g')" \
+    -Dsonar.go.golangci-lint.reportPaths="$OUTDIR/gcilint.out" \
+    -Dsonar.go.coverage.reportPaths="$OUTDIR/coverage.out" \
+    -Dsonar.exclusions="spex/gen/**/*"
+fi
+
 echo "removing temp dir $OUTDIR"
 rm -rf $OUTDIR
